8f534db39c259d1b9ef0fb5f8aba8e043ea8276b
anthonyanderson <tonyanderson526@gmail.com>
2026-07-22 18:57:39 -0400

    fix(characters): restore useAuthStorageListener lost in rebase (#713)

---
+++ apps/web/src/features/auth/index.ts
@@ -0,0 +1,7 @@
+export * from './JoinPage';
+export * from './LoginPage';
+export * from './OidcRecoveryPage';
+export * from './ResetPasswordPage';
+export * from './SetupPage';
+export * from './SignupPage';
+export * from './useAuthStorageListener';
